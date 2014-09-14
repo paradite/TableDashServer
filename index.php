b0858e8af54c29b71d74c9ddfeca7ed2f18dcf1b
Yuan Yiyang <yuan3y@gmail.com>
2014-09-14 16:47:36 +0800

    end of battlehack 2014

--- index.php
+++ index.php
@@ -10,6 +10,8 @@ Up and Dashing!</h2>
 <h3>Server-end check_vacancy function done!</h3>
 <h3>Server-end log_activity function done!</h3>
 <h3>Server-end add_tag_table function done!</h3>
+<p>install the webserver and copy the url to the client side code.<br/>
+You are currently running at <?php echo("http://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]"); ?></p>
 </body>
 </html>
 
--- try.php
+++
@@ -1,6 +0,0 @@
-
-<?php
-$patterns = array ('(null)');
-$replace = array ('"0"');
-echo preg_replace($patterns, $replace, json_encode(new ArrayValue(mysqli_fetch_all($result)), JSON_PRETTY_PRINT));
-?>
