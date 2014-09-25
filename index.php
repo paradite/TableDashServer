8d6fc75775f272882a401d1c47815018aff74876
Yuan Yiyang <yuan3y@gmail.com>
2014-09-26 00:51:06 +0800

    github linkage

--- index.php
+++ index.php
@@ -11,7 +11,8 @@ Up and Dashing!</h2>
 <h3>Server-end log_activity function done!</h3>
 <h3>Server-end add_tag_table function done!</h3>
 <p>install the webserver and copy the url to the client side code.<br/>
-You are currently running at <?php echo("http://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]"); ?></p>
+You are currently running at <?php echo("http://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]"); ?><br/>
+See GitHub: <a href="https://github.com/zhuliangg11/TableDashServer">https://github.com/zhuliangg11/TableDashServer</a></p>
 </body>
 </html>
 
